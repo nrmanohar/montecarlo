bb53471ba34ac74e88acaed4448eadd69d4bdf52
nrmanohar <97973426+nrmanohar@users.noreply.github.com>
2022-04-27 13:25:10 -0400

    Added montecarlo methods and graphs, working well. Still need to properly bughunt and add documentation

--- montecarlo/metropolis.py
+++ montecarlo/metropolis.py
@@ -15,6 +15,9 @@ def average(list):
 
 def sweep(state, mu=1.1, k=1, J=-2, T=1):
     energies = []
+    mags = []
+    states = []
+    states.append(state)
     energies.append(spin_energy(J,mu,k,state))
     sweeper = state.copy()
     for i in range(len(sweeper)):
@@ -23,16 +26,28 @@ def sweep(state, mu=1.1, k=1, J=-2, T=1):
         elif sweeper[i]==1:
             sweeper[i]==-1
         energy_two = spin_energy(J,mu,k,sweeper)
+        avg_eng = sum(energies)/len(energies)
         if energy_two < energies[0]:
             energies.append(energy_two)
+            states.append(sweeper)
         else:
-            prob = math.exp(-energy_two/(k*T))
-            if prob>random.random():
+            prob = math.exp((-energy_two+average(energies))/(k*T))
+            if prob > random.random():
                 energies.append(energy_two)
+                states.append(sweeper)
         sweeper = state.copy()
-    return energies
-def metropolis_sample_energy(n=8, mu=1.1,k=1,J=-2,T=1, sweeps=100):
+    for i in states:
+        mags.append(magnetization(i))
+    data = []
+    data.append(energies)
+    data.append(mags)
+    return data
+
+def metropolis_sample(n=8, mu=1.1,k=1,J=-2,T=1, sweeps=100):
     total_energies = []
+    probabilities = []
+    mags = []
+    states = []
     for i in range(sweeps):
         tester_state = []
         for i in range(n):
@@ -41,20 +56,65 @@ def metropolis_sample_energy(n=8, mu=1.1,k=1,J=-2,T=1, sweeps=100):
                 tester_state.append(-1)
             else:
                 tester_state.append(1)
-        energies = sweep(tester_state,mu,k,J,T)
+        states.append(tester_state)
+        information = sweep(tester_state,mu,k,J,T)
+        energies = information[0]
+        magnet = information[1]
         for i in energies:
             total_energies.append(i)
-    return total_energies
+        for i in magnet:
+            mags.append(i)
+    for i in range(len(total_energies)):
+        probabilities.append(math.exp(-total_energies[i]/(k*T)))
+    normalization = sum(probabilities)
+    for i in range(len(probabilities)):
+        probabilities[i] = probabilities[i]/normalization
+    avg_eng = 0
+    for i in range(len(total_energies)):
+        avg_eng+=(total_energies[i]*probabilities[i])
+    avg_mag = 0
+    for i in range(len(mags)):
+        avg_mag+=(mags[i]*probabilities[i])
+    square_eng = total_energies.copy()
+    for i in range(len(square_eng)):
+        square_eng[i]=(square_eng[i]*square_eng[i])
+    avg_square_eng =0
+    for i in range(len(square_eng)):
+        avg_square_eng+=(square_eng[i]*probabilities[i])
+    heat_cap = (avg_square_eng - (avg_eng**2))/(k*T*T)
+    
+    square_mag = mags.copy()
+    for i in range(len(square_mag)):
+        square_eng[i]=(square_mag[i]*square_mag[i])
+    avg_square_mag =0
+    for i in range(len(square_mag)):
+        avg_square_mag+=(square_mag[i]*probabilities[i])
+    mag_sus = -(avg_square_mag - (avg_mag**2))/(k*T)
+    
+    data = []
+    data.append(avg_eng)
+    data.append(avg_mag)
+    data.append(heat_cap)
+    data.append(mag_sus)
+    return data
 
-def plot(tmin=1,tmax=10,step=0.1,J = -2, mu = 1.1, k = 1, num_states=8,sweeps=1000):
+
+
+def metro_plot(tmin=1,tmax=10,step=0.1,J = -2, mu = 1.1, k = 1, num_states=8,sweeps=1000):
     temps = []
     average_energy = []
+    average_magnetization = []
+    heat_cap = []
+    mag_sus = []
     for i in range(int(tmin),int((tmax-tmin)/step)):
         temps.append(tmin+i*step)
-        energies = metropolis_sample_energy(num_states,mu,k,J,temps[-1],sweeps)
-        average_energy.append(average(energies))
-    plt.plot(temps,average_energy)
-    plt.legend(["Average Energy"],loc='best')
+        data = metropolis_sample(num_states,mu,k,J,temps[-1],sweeps)
+        average_energy.append(data[0])
+        average_magnetization.append(data[1])
+        heat_cap.append(data[2])
+        mag_sus.append(data[3])
+    plt.plot(temps,average_energy,'r-',temps,average_magnetization,'b-',temps,heat_cap,'g-',temps,mag_sus,'y-')
+    plt.legend(["Average Energy", "Average Magnetization", "Heat Capacity", "Magnetic Susceptibility"],loc='best')
     plt.xlabel("Temperature (K)")
     plt.show()
 
